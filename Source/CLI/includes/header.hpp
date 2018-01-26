9de877eee085c179496c3141ee30dc74e11af10e
AustinHodgin <hodgina@oregonstate.edu>
2018-01-26 14:39:37 -0800

    added text out, input from user.

--- Source/CLI/includes/header.hpp
+++ Source/CLI/includes/header.hpp
@@ -1,3 +1,19 @@
 #include <iostream>
+#include <cstdlib> 
+#include<string>
+
 
 using namespace std;
+
+
+int configure_bed ();
+int configure_zone();
+int configure_schedule(int &ColorR, int &ColorG, int &ColorB, int &schedule_number, int &schedule_edit, int &brightness);
+int configure_profile();
+
+int Apply_config();
+
+string build_url();
+
+
+
