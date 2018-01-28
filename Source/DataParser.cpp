f34d634fd330e8ab25d7bb132dd587f52ebef1a9
Zach Lerew <lerewz@oregonstate.edu>
2018-01-28 10:28:14 -0800

    Data parser next id for all objects

--- Source/DataParser.cpp
+++ Source/DataParser.cpp
@@ -4,6 +4,12 @@
 
 // Required for static class members
 unsigned int DataParser::profile_id;
+unsigned int DataParser::zone_id;
+unsigned int DataParser::schedule_id;
+unsigned int DataParser::led_id;
+unsigned int DataParser::led_state_id;
+unsigned int DataParser::daily_state_id;
+unsigned int DataParser::controller_id;
 
 
 // Initialization
@@ -19,5 +25,47 @@ unsigned int DataParser::next_profile_id()
 {
     unsigned int tmp = profile_id;
     profile_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_zone_id()
+{
+    unsigned int tmp = zone_id;
+    zone_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_schedule_id()
+{
+    unsigned int tmp = schedule_id;
+    schedule_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_led_id()
+{
+    unsigned int tmp = led_id;
+    led_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_led_state_id()
+{
+    unsigned int tmp = led_state_id;
+    led_state_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_daily_state_id()
+{
+    unsigned int tmp = daily_state_id;
+    daily_state_id++;
+    return tmp;
+}
+
+unsigned int DataParser::next_controller_id()
+{
+    unsigned int tmp = controller_id;
+    controller_id++;
     return tmp;
 }
