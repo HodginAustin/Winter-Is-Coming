7531b4bc3dd45eb34882cafd390944fed435ae81
carvellwakeman <carvellwakeman@gmail.com>
2018-03-01 19:39:02 -0800

    Other misc updates to relationships

--- Source/includes/relationships.hpp
+++ Source/includes/relationships.hpp
@@ -4,13 +4,30 @@ struct ZoneDOW {
     unsigned int ds_sun_id;
     unsigned int ds_mon_id;
     unsigned int ds_tue_id;
-    unsigned int ds_wed_id;
+    unsigned int ds_wed_id; // It is wednesday, my dudes
     unsigned int ds_thu_id;
     unsigned int ds_fri_id;
     unsigned int ds_sat_id;
 
     std::array<unsigned int, 7> get_days() {
         return {ds_sun_id, ds_mon_id, ds_tue_id, ds_wed_id, ds_thu_id, ds_fri_id, ds_sat_id};
+    }
+
+    unsigned int get_id_null_safe(DailyState* ds)
+    {
+        if (ds) { return ds->get_id(); } else { return 0; }
+    }
+    
+    void set(Zone* z) {
+        zone_id = z->get_id();
+
+        ds_sun_id = get_id_null_safe(z->get_daily_state(0));
+        ds_mon_id = get_id_null_safe(z->get_daily_state(1));
+        ds_tue_id = get_id_null_safe(z->get_daily_state(2));
+        ds_wed_id = get_id_null_safe(z->get_daily_state(3));
+        ds_thu_id = get_id_null_safe(z->get_daily_state(4));
+        ds_fri_id = get_id_null_safe(z->get_daily_state(5));
+        ds_sat_id = get_id_null_safe(z->get_daily_state(6));
     }
 };
 
