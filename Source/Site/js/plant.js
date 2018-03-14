e8a78721f8124297f37318a66c17fe6d84d7639d
Travis Hodgin <hodgint@oregonstate.edu>
2018-03-13 18:40:30 -0700

    Profiles page displays current and all profiles. Navbar redirects to correct page and displays current profile. Some Basic Structure setup.

---
+++ Source/Site/js/plant.js
@@ -0,0 +1,67 @@
+/******************************************************
+* Plant.js - Travis Hodgin,
+* Gets everything needed to render the main plant page
+******************************************************/
+var url = "http://localhost:"; /* base url */
+var port = 9080; /* default port */
+var dailyStates = "daily_states";
+var profiles = "profiles"; /* grab profile */
+var currentProfile = "current_profile";
+var ledStates = "led_states";
+var header = "application/json"; /* header for ajax call */
+
+module.exports = function(){
+  var express = require('express');
+  var router = express.Router();
+  const http = require("http");
+
+    /*gets all profiles. Uses async to collect data and complete to render */
+    function getAllProfiles(res, context, complete){
+      var profileURL = url + port + '/' + profiles;
+      http.get(profileURL, res => {
+        res.setEncoding("utf8");
+        body = "";
+        res.on("data", data => {
+          body += data;
+        });
+        res.on("end", () => {
+          body = JSON.parse(body);
+          context.Profiles = body;
+          complete();
+        });
+      });
+    }
+
+    /*gets current profile. Uses async to collect data and complete to render */
+    function getCurrentProfile(res, context, complete){
+      var current = url + port + '/' + currentProfile;
+      http.get(current, res => {
+        res.setEncoding("utf8");
+        body = "";
+        res.on("data", data => {
+          body += data;
+        });
+        res.on("end", () => {
+          body = JSON.parse(body);
+          context.currentProfile = body;
+          complete();
+        });
+      });
+    }
+
+    router.get('/', function(req, res){
+      var callbackCount = 0;
+      var context = {};
+      getAllProfiles(res, context, complete);
+      getCurrentProfile(res, context, complete);
+      function complete(){
+        console.log("in complete");
+        callbackCount++;
+        if(callbackCount >= 2){
+          console.log("");
+          res.render('plant', context);
+        }
+      }
+    });
+  return router;
+}();
---
+++ Source/Site/main.js
@@ -0,0 +1,39 @@
+var express = require('express'); /*express for handlebars*/
+var handlebars = require('express-handlebars'); /* templating engine */
+var bodyParser = require('body-parser');
+const path = require("path"); /* allows use of path.join for folder paths */
+var app = express();
+
+
+app.engine('handlebars', handlebars({
+	 defaultLayout: 'main',
+}));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static(path.join(__dirname, 'public'))); /* public folder */
+app.use('/js', express.static(path.join(__dirname, 'js'))); /* Javascript folder */
+app.use('/css', express.static(path.join(__dirname, 'css'))); /* style sheets folder */
+app.use('/img', express.static(path.join(__dirname, 'img'))); /* images folder */
+
+
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]); /* sets port to what is given in command line */
+
+app.use('/plant', require('./js/plant.js'));
+app.use('/profiles', require('./js/profiles.js'));
+app.use('/zones', require('./js/zones.js'));
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(req,res){
+  res.status(500);
+  res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
