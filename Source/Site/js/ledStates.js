141ce10cf0e0df838df85f6c3ca95cd586a0322c
carvellwakeman <carvellwakeman@gmail.com>
2018-03-16 12:10:50 -0700

    LED States base

--- Source/Site/js/ledStates.js
+++ Source/Site/js/ledStates.js
@@ -9,60 +9,77 @@ var profiles = "profiles"; /* grab profile */
 var currentProfile = "current_profile";
 var ledStates = "led_states";
 
-module.exports = function(){
+module.exports = function () {
   var express = require('express');
   var router = express.Router();
   const http = require("http");
 
-    /*gets all profiles. Uses async to collect data and complete to render */
-    function getAllProfiles(res, context, complete){
-      var profileURL = url + port + '/' + profiles;
-      http.get(profileURL, res => {
-        res.setEncoding("utf8");
-        body = "";
-        res.on("data", data => {
-          body += data;
-        });
-        res.on("end", () => {
-          body = JSON.parse(body);
-          context.Profiles = body;
-          complete();
-        });
+  /*gets all profiles. Uses async to collect data and complete to render */
+  function getAllProfiles(res, context, complete) {
+    var profileURL = url + port + '/' + profiles;
+    http.get(profileURL, res => {
+      res.setEncoding("utf8");
+      body = "";
+      res.on("data", data => {
+        body += data;
       });
-    }
-
-    /*gets current profile. Uses async to collect data and complete to render
-      THIS FUNCTION SHOULD BE INCLUDED IN ALL PAGES FOR THE NAVBAR CURRENT PROFILE*/
-    function getCurrentProfile(res, context, complete){
-      var current = url + port + '/' + currentProfile;
-      http.get(current, res => {
-        res.setEncoding("utf8");
-        body = "";
-        res.on("data", data => {
-          body += data;
-        });
-        res.on("end", () => {
-          body = JSON.parse(body);
-          context.currentProfile = body;
-          complete();
-        });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        context.Profiles = body;
+        complete();
       });
-    }
-
-    router.get('/', function(req, res){
-      var callbackCount = 0;
-      var context = {};
-      getAllProfiles(res, context, complete);
-      getCurrentProfile(res, context, complete);
-      function complete(){
-        console.log("in complete");
-        callbackCount++;
-        if(callbackCount >= 2){
-          console.log("ZONES!");
-          res.render('ledStates', context);
-        }
-      }
     });
+  }
+
+  function getAllLEDStates(res, context, complete) {
+    var ledStatesUrl = url + port + '/' + ledStates;
+    http.get(ledStatesUrl, res => {
+      res.setEncoding("utf8");
+      body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        context.LEDStates = body;
+        complete();
+      });
+    });
+  }
+
+  /*gets current profile. Uses async to collect data and complete to render
+    THIS FUNCTION SHOULD BE INCLUDED IN ALL PAGES FOR THE NAVBAR CURRENT PROFILE*/
+  function getCurrentProfile(res, context, complete) {
+    var current = url + port + '/' + currentProfile;
+    http.get(current, res => {
+      res.setEncoding("utf8");
+      body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        context.currentProfile = body;
+        complete();
+      });
+    });
+  }
+
+  router.get('/', function (req, res) {
+    var callbackCount = 0;
+    var context = {};
+    getAllLEDStates(res, context, complete);
+    getAllProfiles(res, context, complete);
+    getCurrentProfile(res, context, complete);
+    function complete() {
+      console.log("in complete");
+      callbackCount++;
+      if (callbackCount >= 3) {
+        console.log("LEDStates!");
+        res.render('ledStates', context);
+      }
+    }
+  });
 
 
 
