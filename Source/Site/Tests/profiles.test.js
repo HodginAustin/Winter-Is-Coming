5a726255e83565614d313b9b5db4bf81c40ccffa
Travis Hodgin <hodgint@oregonstate.edu>
2018-04-21 15:26:31 -0700

    Fixed some testing issues (STILL NEEDS IMPLEMENTATION).

--- Source/Site/Tests/profiles.test.js
+++ Source/Site/Tests/profiles.test.js
@@ -1,13 +1,25 @@
-var chai = require('chai');
 var profilesJS = require('../js/profiles.js');
 let conn = require('../js/global/connection.js');
 let controlService = require('../js/global/controlServiceRequest.js');
+var chai = require('chai');
+var sinon = require('sinon');
+var express = require('express');
+var supertest = require('supertest');
+
+
 
 var i = 1;
 
 describe("Profiles", function(){
+  var app, request, route, getProfileStub;
   beforeEach(function(done){
     let j = {};
+    app = express();
+    route = proxyquire('../js/profiles.js', {
+      get
+    });
+    route(app);
+    request = supertest(app);
     j['name'] = "Test Name";
     j['description'] = "Test Description";
     var options = {
@@ -16,7 +28,7 @@ describe("Profiles", function(){
         json: j
     };
     controlService.makeRequest(options, function () {});
-
+    done();
   });
 
   afterEach(function(done){
@@ -26,7 +38,7 @@ describe("Profiles", function(){
         uri: conn.url + conn.port + "/" + conn.profiles + "/" + id + "/delete"
     };
     controlService.makeRequest(options, function () {});
-     i++;
+     done();
   });
 
   it('should return all profiles', function(done){
@@ -39,16 +51,19 @@ describe("Profiles", function(){
   });
 
   it('get single profile should return status 200', function(done){
+    done();
 
   });
   it('edit profile should return status 200', function(done){
+    done();
 
   });
   it('delete profile should return status 200', function(done){
+    done();
 
   });
   it('should add a profile and give status 200', function(done){
-
+    done();
   });
 
 });
