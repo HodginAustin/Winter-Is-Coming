f06d66dbd5eee57e0388f4b061a321e7a0d3ac93
carvellwakeman <carvellwakeman@gmail.com>
2018-04-21 11:15:31 -0700

    Changed everyboot to run from directory

--- Source/setup/everyboot.py
+++ Source/setup/everyboot.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Start plantergb
-subprocess.call("sudo /plantergb/out/plantergb &", shell=True)
+subprocess.call("cd /plantergb/out/ && ./plantergb &", shell=True)
 
 # Start plantergb website
-subprocess.call("sudo npm start --prefix /plantergb/Site &", shell=True)
+subprocess.call("npm start --prefix /plantergb/Site &", shell=True)
