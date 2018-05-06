ba1dc4dbe47c232902213861daa0d7108e246ab5
carvellwakeman <carvellwakeman@gmail.com>
2018-05-06 14:17:28 -0700

    Fixed simulator deleting beds

--- Source/Site/js/simulator.js
+++ Source/Site/js/simulator.js
@@ -12,12 +12,13 @@ module.exports = function() {
 
     // API - takes data in to display
     router.post('/update/:io/:r/:g/:b/:strip_idx', function(req, res) {
+        
         let io = parseInt(req.params.io);
         let r = parseInt(req.params.r);
         let g = parseInt(req.params.g);
         let b = parseInt(req.params.b);
         let strip_idx = parseInt(req.params.strip_idx);
-
+        
         if (io >= 0 && r >= 0 && g >= 0 && b >= 0 && strip_idx >= 0) {
             if (beds[io - 1] && beds[io - 1].leds[strip_idx]) {
                 beds[io - 1].leds[strip_idx].r = r;
@@ -34,18 +35,28 @@ module.exports = function() {
         return;
     });
 
+    function maxBedId() {
+        let m = 0;
+        beds.forEach(function(b) {
+            m = Math.max(b.id, m);
+        });
+        return m;
+    }
+
     router.post('/putBed/', function(req, res) {
         let bed = {};
-        if (req.query.bedId && beds[req.query.bedId]) {
-            bed = beds[parseInt(req.query.bedId)];
-            bed["leds"] = [];
+        let f = beds.filter(b => b.id == req.query.bedId);
+        if (req.query.bedId && f.length > 0) {
+            bed = f[0];
+            bed.leds = [];
         } else {
-            beds.push({ "id": beds.length, "leds": [] });
-            bed = beds[beds.length - 1];
+            let m = maxBedId()+1;
+            beds.push({ "id": m, "leds": [] });
+            bed = beds.filter(b => b.id == m)[0];
         }
         if (req.body.leds) {
             for (let i = 0; i < parseInt(req.body.leds); i++) {
-                bed["leds"].push({ r: 0, g: 0, b: 0, strip_idx: i });
+                bed.leds.push({ r: 0, g: 0, b: 0, strip_idx: i });
             }
 
             res.status(200).json({ id: bed.id });
@@ -56,9 +67,11 @@ module.exports = function() {
 
     router.delete('/deleteBed/:bedId', function(req, res) {
         if (req.params.bedId) {
-            if (beds[req.params.bedId]) {
-                beds[req.params.bedId] = null;
+            if (beds.filter(b => b.id == req.params.bedId).length > 0) {
+                beds = beds.filter(b => b.id != req.params.bedId);
+
                 res.status(201).send("success");
+                return;
             }
             res.status(404).send("BedID not found");
             return;
