011c6ff8a5a56168458f031569cf0db7d41a6850
cascadeth <cascadeth@gmail.com>
2018-05-06 17:55:50 -0700

    Added serial write back for values; update StateComposer to allow more time for nano to complete

--- Source/StateComposer.cpp
+++ Source/StateComposer.cpp
@@ -20,8 +20,8 @@
 
 // The amount of microseconds to wait for the
 // Arduino Nano to send it's ACK and catch up
-// last updated to: 20000
-#define WAIT 20000
+// last updated to: 35000 (for serial write on nano)
+#define WAIT 35000
 
 // Defines the function that should be used to send serial data
 // serial_send is used to send over I2C to hardware
